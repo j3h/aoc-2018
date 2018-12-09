156b3cf4b08c9a6d8f2d56999395e6f7a75efdca
Josh Barnes-Hoyt <joshhoyt@gmail.com>
2018-12-09 14:32:00 -0800

    problem 1 solved

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+use std::io::BufReader;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let input = stdin.lock();
+    let mut total = 0;
+    for line_res in BufReader::new(input).lines() {
+        let line = line_res.unwrap();
+        let (op, arg_str) = line.split_at(1);
+        let arg = arg_str.parse::<i32>().unwrap();
+        match op {
+            "-" => total = total - arg,
+            "+" => total = total + arg,
+            other => panic!(format!("invalid op: {}", other))
+        };
+    }
+    println!("{}", total);
 }
