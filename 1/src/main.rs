751d989c6636de8e935ebd6ab9b84e93d2d95c95
Josh Barnes-Hoyt <joshhoyt@gmail.com>
2018-12-09 20:37:27 -0800

    day 1 complete

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,20 +1,33 @@
 use std::io;
 use std::io::BufReader;
 use std::io::BufRead;
+use std::collections::BTreeSet;
 
 fn main() {
     let stdin = io::stdin();
     let input = stdin.lock();
-    let mut total = 0;
-    for line_res in BufReader::new(input).lines() {
+    let mut seen = BTreeSet::new();
+    let deltas: Vec<i32> =
+    BufReader::new(input).lines().map(|line_res| {
         let line = line_res.unwrap();
         let (op, arg_str) = line.split_at(1);
         let arg = arg_str.parse::<i32>().unwrap();
         match op {
-            "-" => total = total - arg,
-            "+" => total = total + arg,
+            "-" => -arg,
+            "+" => arg,
             other => panic!(format!("invalid op: {}", other))
-        };
+        }
+    }).collect();
+        let mut total = 0;
+    let mut i = 0;
+    for d in deltas.iter().cycle() {
+        if seen.contains(&total) {
+            break;
+        } else {
+            seen.insert(total);
+        }
+        i += 1;
+        total += d;
     }
-    println!("{}", total);
+    println!("after {} ({} deltas), found {}", i, deltas.len(), total);
 }
